d24755311e1bd24da860d94be00b389902f2c35b
Governor360 <116545140+Governor360@users.noreply.github.com>
2023-08-08 08:31:10 +0100

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_8w0@ugm#%a359d_oe^bu1j+g70pnvs%0*m+u+$a&1qwl&yc(l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.82.158.148']
+ALLOWED_HOSTS = ['34.227.178.38']
 
 
 # Application definition
